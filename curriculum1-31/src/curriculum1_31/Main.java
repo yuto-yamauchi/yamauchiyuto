b169382b3db7a599c0d63a9e0edaf77fc579cf34
yamauchiyuto <12.yamauchi.yuto.23@gmail.com>
2023-07-26 15:10:49 +0900

    curriculum1-31修正

--- curriculum1-31/src/curriculum1_31/Main.java
+++ curriculum1-31/src/curriculum1_31/Main.java
@@ -2,9 +2,9 @@ package curriculum1_31;
 
 public class Main {
 	public static void main(String[] args) {
-		//　インスタンス生成
-		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);		
-		Person person2 = new Person("山田花子", 22, 1.5, 40);
+		//　インスタンス生成2
+		Person person1 = new Person("鈴木","太郎", 20, 1.7, 60);
+		Person person2 = new Person("山田","花子", 22, 1.5, 40);
 		Car car = new Car();
 		Bicycle bicycle = new Bicycle();
 		// 各名前のセット、コンソール出力
